5da3dddce34b9d6de92e62fe9b1af1bd8c187b71
ladazkalii <ladazkalii@gmail.com>
2024-12-04 05:58:00 +0700

    Create lor.py

---
+++ lor.py
@@ -0,0 +1,40 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+options = {
+    'proxy': {
+        'http': 'http://ae1f7c1abc21d6907425__cr.sg:83fc052021c1be3f@gw.dataimpulse.com:10100',
+        'https': 'https://ae1f7c1abc21d6907425__cr.sg:83fc052021c1be3f@gw.dataimpulse.com:10100',
+        'no_proxy': 'localhost,127.0.0.1'
+    }
+}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--verbose")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920, 1200")
+# Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
+
+driver.get('https://httpbin.org/ip')
+print(driver.page_source)
+
+time.sleep(1)
+
+driver.get("https://sepolia-faucet.pk910.de/#/mine/ee10e7fe-3a98-475b-bb36-945a1923ce15")
+time.sleep(50)
+
+div_element = driver.find_element(By.CLASS_NAME, "col-3")
+content_text = div_element.text
+print(content_text)
+
+#WAKTU MENUNGGU MINING SELESAI
+time.sleep(15000)
+
+# Tutup browser
+driver.quit()
